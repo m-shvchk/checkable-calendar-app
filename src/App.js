5d71ed1c8cb049efc86d2e46dba17ab1e8c28c7e
m-shvchk <thirdinthequeue@gmail.com>
2022-04-22 23:52:05 +0300

    working with mouse selection

--- src/App.js
+++ src/App.js
@@ -54,6 +54,10 @@ Object.entries(initialData).map(([day, value]) => {
 const App = () => {
   const [normalized, setNormalized] = useState(normalizedArr);
 
+  const [mouseIsActive, setMouseIsActive] = useState(false);
+  const [selectionStart, setSelectionStart] = useState();
+  const [selectionEnd, setSelectionEnd] = useState();
+
   const clearAllHandler = () => {
     normalized.map((val) => {
       val.fill(false);
@@ -61,13 +65,71 @@ const App = () => {
     setNormalized([...normalized]);
   };
 
+  const mouseDownHandler = (e) => {
+    const id = e.target.id;
+    if (!id) return;
+    const [x, y] = id.split("-");
+    if (normalized[x][y]) return;
+    setSelectionStart({ x, y });
+    setMouseIsActive(true);
+  };
+
+  const mouseMoveHandler = (e) => {
+    if (!mouseIsActive) return;
+    const id = e.target.id;
+    if (id) {
+      const [x, y] = id.split("-");
+      setSelectionEnd({ x, y });
+      
+      // let x1, x2, y1, y2;
+      // if (selectionStart.x < selectionEnd.x || selectionStart.y < selectionEnd.y){
+      //   x1 = selectionStart.x;
+      //   y1 = selectionStart.y;
+      //   x2 = selectionEnd.x;
+      //   y2 = selectionEnd.y;
+      // } else {
+      //   x1 = selectionEnd.x;
+      //   y1 = selectionEnd.y;
+      //   x2 = selectionStart.x;
+      //   y2 = selectionStart.y;
+      // }
+      // console.log(x1, y1, x2, y2)
+      // let numToBeChecked = (x2 - x1) * 24 + y2 - y1 + 1;
+      // let x_ = x1;
+      // let y_ = y1;
+      // for (let i = 0; i < numToBeChecked; i++) {
+      //   normalized[x_][y_] = true;
+      //   setNormalized([...normalized]);
+      //   if (y_ < 23) {
+      //     y_ = y_ + 1;
+      //   }
+        
+      // }
+
+      let x1 = Math.min(selectionStart.x, selectionEnd.x);
+      let x2 = Math.max(selectionStart.x, selectionEnd.x);
+      let y1 = Math.min(selectionStart.y, selectionEnd.y);
+      let y2 = Math.max(selectionStart.y, selectionEnd.y);
+      for (let i = x1; i <= x2; i++) {
+        for (let k = y1; k <= y2; k++) {
+          normalized[i][k] = true;
+        }
+      }
+      setNormalized([...normalized]);
+    }
+  };
+  // console.log(selectionStart)
+  // console.log(selectionEnd)
+
+  const mouseUpHandler = (e) => {
+    setMouseIsActive(false);
+    setSelectionStart("");
+    setSelectionEnd("");
+  };
+
   let content = normalized.map((hours, dayIndex) => (
     <div className={classes.flex_row} key={dayIndex}>
-      <div
-        className={classes.day_column}
-        id={numToDay[dayIndex]}
-        key={numToDay[dayIndex]}
-      >
+      <div className={classes.day_column} key={numToDay[dayIndex]}>
         {numToDay[dayIndex].toUpperCase()}
       </div>
 
@@ -82,6 +144,7 @@ const App = () => {
         <DataCell
           value={val}
           key={`${dayIndex}-${hourIndex}`}
+          id={`${dayIndex}-${hourIndex}`}
           dayIndex={dayIndex}
           hourIndex={hourIndex}
           normalized={normalized}
@@ -93,7 +156,12 @@ const App = () => {
 
   return (
     <Fragment>
-      <div className={classes.container}>
+      <div
+        className={classes.container}
+        onMouseDown={mouseDownHandler}
+        onMouseMove={mouseMoveHandler}
+        onMouseUp={mouseUpHandler}
+      >
         <h2>SET SCHEDULE</h2>
         <Header />
         {content}
@@ -110,3 +178,10 @@ const App = () => {
 };
 
 export default App;
+
+// let x1 = Math.min(selectionStart.x, selectionEnd.x);
+// let x2 = Math.max(selectionStart.x, selectionEnd.x);
+// let y1 = Math.min(selectionStart.y, selectionEnd.y);
+// let y2 = Math.max(selectionStart.y, selectionEnd.y);
+
+
--- src/components/DataCell.js
+++ src/components/DataCell.js
@@ -1,7 +1,7 @@
 import React from "react";
 import classes from "./DataCell.module.css";
 
-const DataCell = ({ value, normalized, setNormalized, dayIndex, hourIndex }) => {
+const DataCell = ({ value, normalized, setNormalized, dayIndex, hourIndex, id }) => {
   const checkCellHandler = () => {
     normalized[dayIndex][hourIndex] = !value;
     setNormalized([...normalized])
@@ -11,7 +11,7 @@ const DataCell = ({ value, normalized, setNormalized, dayIndex, hourIndex }) => 
     ? `${classes.hour__cell_checked}`
     : `${classes.hour__cell}`;
 
-  return <div className={checked_class} onClick={checkCellHandler}></div>;
+  return <div className={checked_class} id={id} onClick={checkCellHandler}></div>;
 };
 
 export default DataCell;
